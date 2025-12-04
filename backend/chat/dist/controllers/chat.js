dcc461d85633a0a7802a0dbaa20f3f956aab3d3d
moabia1 <mdmoabia81@gmail.com>
2025-12-04 15:09:13 +0530

    Chat service completed only we have to implement socket.io

--- backend/chat/dist/controllers/chat.js
+++ backend/chat/dist/controllers/chat.js
@@ -65,6 +65,120 @@ export const getAllChats = tryCatch(async (req, res) => {
 export const sendMessage = tryCatch(async (req, res) => {
     const senderId = req.user?._id;
     const { chatId, text } = req.body;
-    // const imageFile = req.file
+    const imageFile = req.file;
+    if (!senderId) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+    if (!chatId) {
+        res.status(400).json({ message: "ChatId required" });
+        return;
+    }
+    if (!text && !imageFile) {
+        res.status(400).json({ message: "Either text or image required" });
+        return;
+    }
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+        res.status(404).json({
+            message: "Chat not found"
+        });
+        return;
+    }
+    const isUserInChat = chat.users.some((userId) => userId.toString() === senderId.toString());
+    if (!isUserInChat) {
+        res.status(403).json({ message: "you are not a participant of this chat" });
+        return;
+    }
+    const otherUserId = chat.users.find(id => id !== senderId);
+    if (!otherUserId) {
+        res.status(401).json({ message: "no other user" });
+        return;
+    }
+    // Socket setup
+    let messageData = {
+        chatId: chatId,
+        sender: senderId,
+        seen: false,
+        seenAt: undefined
+    };
+    if (imageFile) {
+        messageData.image = {
+            url: imageFile.path,
+            publicId: imageFile.filename
+        };
+        messageData.messageType = "image",
+            messageData.text = text || "";
+    }
+    else {
+        messageData.text = text,
+            messageData.messageType = "text";
+    }
+    const message = new Messages(messageData);
+    const savedMessage = await message.save();
+    const latestMessage = imageFile ? "📷 Image" : text;
+    await Chat.findByIdAndUpdate(chatId, {
+        latestMessage: {
+            text: latestMessage,
+            sender: senderId
+        },
+        updatedAt: new Date()
+    }, { new: true });
+    //emit
+    res.status(201).json({
+        message: savedMessage,
+        sender: senderId
+    });
+});
+export const getMessagesByChat = tryCatch(async (req, res) => {
+    const userId = req.user?._id;
+    const { chatId } = req.params;
+    if (!userId) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
+    }
+    if (!chatId) {
+        res.status(401).json({ message: "chatId required" });
+        return;
+    }
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+        res.status(404).json({ message: "chat not found" });
+        return;
+    }
+    const isUserInChat = chat.users.some((user) => user === userId);
+    if (!isUserInChat) {
+        res.status(403).json({ message: "you are not a participant of this chat" });
+        return;
+    }
+    const messageMarkSeen = await Messages.find({
+        chatId: chatId,
+        sender: { $ne: userId },
+        seen: false
+    });
+    await Messages.updateMany({ chatId: chatId, sender: { $ne: userId }, seen: false }, {
+        seen: true,
+        seenAt: new Date()
+    });
+    const messages = await Messages.find({ chatId: chatId }).sort({ createdAt: 1 });
+    const otherUserId = chat.users.find(id => id !== userId);
+    try {
+        if (!otherUserId) {
+            res.status(400).json({ message: "No Other user" });
+            return;
+        }
+        const { data } = await axios.get(`${process.env.USER_SERVICE}/api/v1/user/${otherUserId}`);
+        res.json({
+            messages,
+            user: data
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.json({
+            messages,
+            user: { _id: otherUserId, name: "Unknowm user" }
+        });
+    }
 });
 //# sourceMappingURL=chat.js.map
--- backend/chat/dist/routes/chat.route.js
+++ backend/chat/dist/routes/chat.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { createNewChat, getAllChats } from "../controllers/chat.js";
+import { createNewChat, getAllChats, getMessagesByChat, sendMessage } from "../controllers/chat.js";
+import { upload } from "../middlewares/multer.js";
 const router = express.Router();
 router.post("/chat/new", isAuth, createNewChat);
 router.get("/chats/all", isAuth, getAllChats);
+router.post("/message", isAuth, upload.single("image"), sendMessage);
+router.get("/message/:chatId", isAuth, getMessagesByChat);
 export default router;
 //# sourceMappingURL=chat.route.js.map
