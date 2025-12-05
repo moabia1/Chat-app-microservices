8f51e8cf128f05246b176eabebc9af5d10ed803d
moabia1 <mdmoabia81@gmail.com>
2025-12-05 23:43:31 +0530

    start working on chat app

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,23 +1,48 @@
-"use client"
-import Loading from '@/components/Loading'
-import { useAppData } from '@/context/AppContext'
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+"use client";
+import Loading from "@/components/Loading";
+import { useAppData, User } from "@/context/AppContext";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+export interface Message {
+  _id: string;
+  chatId: string;
+  sender: string,
+  text?: string;
+  image?: {
+    url: string;
+    publicId:string
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt:string;
+}
+
 
 const ChatApp = () => {
-  const { loading, isAuth } = useAppData()
+  const { loading, isAuth,logoutUser,chats,user:loggeinUser,users,fetchChats,setChats } = useAppData();
+
+  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [message, setMessage] = useState("")
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [messages, setMessages] = useState<Message[] | null>(null);
+  const [user, setUser] = useState<User | null>(null)
+  const [showAllUsers, setShowAllUsers] = useState(false)
+  const [isTyping, setIsTyping] = useState(false)
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
 
   const router = useRouter();
 
   useEffect(() => {
     if (!isAuth && !loading) {
-      router.push("/login")
+      router.push("/login");
     }
-  }, [isAuth, router, loading])
-  if(loading) return <Loading/>
-  return (
-    <div>ChatApp</div>
-  )
-}
+  }, [isAuth, router, loading]);
+  if (loading) return <Loading />;
+  return <div className="min-h-screen flex bg-gray-900 text-white relative overflow-hidden">
+    
+  </div>;
+};
 
-export default ChatApp
+export default ChatApp;
